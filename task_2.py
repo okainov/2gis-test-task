543b32de782e16752cd8cacc7f2c425e4686d4ae
Oleg Kainov <kainov.oleg@gmail.com>
2019-12-14 18:00:55 +0100

    cat: add base implementation

--- task_2.py
+++ task_2.py
@@ -17,8 +17,18 @@ class Cat:
     def __init__(self, initial_state: CatState = CatState.HUNGRY):
         self._state = initial_state
 
-    def input(self, input_symbol: CatInput):
-        pass
+    def input(self, input_symbol: str):
+        if self.state == Cat.CatState.HUNGRY and input_symbol == Cat.CatInput.DOG.value:
+            self.action_run()
+        elif self.state == Cat.CatState.HUNGRY and input_symbol == Cat.CatInput.SALAMI.value:
+            self._state = Cat.CatState.FULL
+            self.action_eat()
+        elif self.state == Cat.CatState.FULL and input_symbol == Cat.CatInput.SALAMI.value:
+            self.action_sleep()
+            self._state = Cat.CatState.HUNGRY
+        elif self.state == Cat.CatState.FULL and input_symbol == Cat.CatInput.DOG.value:
+            self.action_run()
+            self._state = Cat.CatState.HUNGRY
 
     def action_run(self):
         print('Run!')
--- test_task_2.py
+++ test_task_2.py
@@ -1,12 +1,53 @@
 from unittest import TestCase
+from unittest.mock import MagicMock
 
 from task_2 import Cat
 
 
 class TestCat(TestCase):
-
-    def setUp(self) -> None:
-        self.cat = Cat()
-
     def test_default_state_hungry(self):
-        self.assertEqual(self.cat.state, Cat.CatState.HUNGRY)
+        self.assertEqual(Cat().state, Cat.CatState.HUNGRY)
+
+    def test_run_when_hungry_and_dog(self):
+        cat = Cat(Cat.CatState.HUNGRY)
+        cat.action_run = MagicMock()
+        cat.input('dog')
+        cat.action_run.assert_called_once()
+
+    def test_stays_hungry_when_hungry_and_dog(self):
+        cat = Cat(Cat.CatState.HUNGRY)
+        cat.input('dog')
+        self.assertEqual(cat.state, cat.CatState.HUNGRY)
+
+    def test_eat_when_hungry_and_salami(self):
+        cat = Cat(Cat.CatState.HUNGRY)
+        cat.action_eat = MagicMock()
+        cat.input('salami')
+        cat.action_eat.assert_called_once()
+
+    def test_goes_to_full_when_hungry_and_salami(self):
+        cat = Cat(Cat.CatState.HUNGRY)
+        cat.input('salami')
+        self.assertEqual(cat.state, cat.CatState.FULL)
+
+    def test_sleep_when_full_and_salami(self):
+        cat = Cat(Cat.CatState.FULL)
+        cat.action_sleep = MagicMock()
+        cat.input('salami')
+        cat.action_sleep.assert_called_once()
+
+    def test_goes_to_hungry_when_full_and_salami(self):
+        cat = Cat(Cat.CatState.FULL)
+        cat.input('salami')
+        self.assertEqual(cat.state, cat.CatState.HUNGRY)
+
+    def test_goes_to_hungry_when_full_and_dog(self):
+        cat = Cat(Cat.CatState.FULL)
+        cat.input('dog')
+        self.assertEqual(cat.state, cat.CatState.HUNGRY)
+
+    def test_run_when_full_and_dog(self):
+        cat = Cat(Cat.CatState.FULL)
+        cat.action_run = MagicMock()
+        cat.input('dog')
+        cat.action_run.assert_called_once()
